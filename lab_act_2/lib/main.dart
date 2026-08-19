77f069aa6fc055798686d94fb946f4cbadbbcbe0
LAB12_USER ON WS22 <L12X16W22@LabHCI.net>
2026-08-19 17:58:44 +0800

    Added gradient background

--- lab_act_2/lib/main.dart
+++ lab_act_2/lib/main.dart
@@ -4,7 +4,9 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(child: Center(child: Text("Hello World"))),
+        body: Container(child: Center(child: Text("Hello World")), 
+        decoration: BoxDecoration(gradient: LinearGradient(colors: [Colors.purple, Colors.pink])),
+        ),
       ),
     ),
   );
